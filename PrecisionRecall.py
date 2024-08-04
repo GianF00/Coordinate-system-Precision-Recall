0edf1e14109cd58516c9ca64e40842c798c4e1b6
GianF00 <98825997+GianF00@users.noreply.github.com>
2024-08-05 00:51:44 +0200

    PrecisionRecall

--- PrecisionRecall.py
+++ PrecisionRecall.py
@@ -33,17 +33,17 @@ data = [
     {"recall": 0.8333333333333334, "precision": 0.7142857142857143, "label": "GPT4 + Grounding DINO 23% (gt_bordny8_ny.jpg)"},
     {"recall": 0.5, "precision": 1.0, "label": "GPT4 + Grounding DINO 37% (gt_bordny8_ny.jpg)"},
 
-    # gt_bordny8_ny.jpg with LLaVA
+    # gt_bordny8_ny.jpg
     {"recall": 0.8333333333333334, "precision": 0.125, "label": "LLaVA v1.6 + Grounding DINO 12% (gt_bordny8_ny.jpg)"},
     {"recall": 0.8333333333333334, "precision": 0.625, "label": "LLaVA v1.6 + Grounding DINO 23% (gt_bordny8_ny.jpg)"},
     {"recall": 0.8333333333333334, "precision": 0.5555555555555556, "label": "LLaVA v1.6 + Grounding DINO 37% (gt_bordny8_ny.jpg)"},
 
-    # gt_ordning14.jpg with LLaVA
+    # gt_ordning14.jpg
     {"recall": 0.8, "precision": 0.25, "label": "LLaVA v1.6 + Grounding DINO 12% (gt_ordning14.jpg)"},
     {"recall": 0.8, "precision": 0.6666666666666666, "label": "LLaVA v1.6 + Grounding DINO 23% (gt_ordning14.jpg)"},
     {"recall": 0.6, "precision": 1.0, "label": "LLaVA v1.6 + Grounding DINO 37% (gt_ordning14.jpg)"},
 
-    # gt_ordning15.jpg with LLaVA
+    # gt_ordning15.jpg
     {"recall": 1.0, "precision": 0.15789473684210525, "label": "LLaVA v1.6 + Grounding DINO 12% (gt_ordning15.jpg)"},
     {"recall": 1.0, "precision": 0.4286, "label": "LLaVA v1.6 + Grounding DINO 23% (gt_ordning15.jpg)"},
     {"recall": 0.6666666666666666, "precision": 1.0, "label": "LLaVA v1.6 + Grounding DINO 37% (gt_ordning15.jpg)"},
